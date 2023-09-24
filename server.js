71c0ca73712e62a3bac8a6c46317bb9a72438868
JUST-VAMSI <104960009+JUST-VAMSI@users.noreply.github.com>
2023-09-24 15:12:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.post("/login",async(req,res)=>{
             if(passwordCheck)
             {
                 const token = jwt.sign({email},"jwt-secret-key",{expiresIn:"1d"});
-                res.cookie("token",token,{maxAge:86400000, httpOnly: true, secure: true,sameSite:'None' });
+                res.cookie("token",token,{maxAge:30000, httpOnly: true, secure: true,sameSite:'None' });
                 return res.json({status:"success",tok:token});
             }
             else{
