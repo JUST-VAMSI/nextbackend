147cb24c94e6832fedbe4c549685f154515fa7a5
JUST-VAMSI <104960009+JUST-VAMSI@users.noreply.github.com>
2023-09-24 11:52:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.post("/login",async(req,res)=>{
             {
                 const token = jwt.sign({email},"jwt-secret-key",{expiresIn:'1d'});
                 // res.cookie('token',token);
-                res.cookie('token',1254678);
+                await res.cookie('token',1254678);
                 console.log("Cookie set");
                 return res.json("success");
             }
